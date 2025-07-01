d4697fe5afac88ca9cbcdaf8b7261f39cbcca2b8
Rohan Chougule <rohanchougule090@gmail.com>
2025-07-01 17:37:38 +0530

    super setup tools half working done

---
+++ src/pages/super/setup_tools/PortalSetting.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PortalSetting = () => {
+  return <div>PortalSetting</div>;
+};
---
+++ src/pages/super/setup_tools/QuickLinks.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const QuickLinks = () => {
+  return <div>QuickLinks</div>;
+};
